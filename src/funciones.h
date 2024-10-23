7a074ef632f133efd70c31ec3c37858ce6cf215b
AlejandroColl006 <acolllen@myuax.com>
2024-10-23 19:58:03 +0200

    wip

--- src/funciones.h
+++ src/funciones.h
@@ -5,6 +5,12 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#ifdef NOCIONESCLAVE_LIB_EXPORT
+    #define NOCIONESCLAVE_API __declspec(dllexport)
+#else
+    #define NOCIONESCLAVE_API __declspec(dllimport)
+#endif
+
 void muestraHolaMundo();
 
 #endif //FUNCIONES_H
